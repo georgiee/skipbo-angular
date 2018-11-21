35dc0e7351c82de521ca70cd53ceb856cb899475
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-21 18:44:54 +0100

    completed 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { GameModule } from './game/game.module';
+import { GameService } from './game/services/game.service';
 
 @NgModule({
   declarations: [
@@ -9,9 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    GameModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
