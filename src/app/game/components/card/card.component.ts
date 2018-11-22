90a73f871ef3b88645db09b7ac1f1fb75ab2d44b
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-22 17:39:20 +0100

    update

--- src/app/game/components/card/card.component.ts
+++ src/app/game/components/card/card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Host, Optional } from '@angular/core';
 import { Card } from 'skipbo-core';
+import { CardPileComponent } from '../card-pile/card-pile.component';
 
 @Component({
   selector: 'skipbo-card',
@@ -7,12 +8,21 @@ import { Card } from 'skipbo-core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  constructor() { }
   @Input() value: Card;
   @Input() revealed: boolean = false;
 
   get currentFace() {
     return this.revealed ? this.value : Card.Back;
+  }
+  constructor(
+    @Host() @Optional() pile: CardPileComponent
+  ) {
+
+    if (pile) {
+      console.log('Card says: My parent pile is', pile);
+    } else {
+      console.log('Card says: I have no parent pile');
+    }
   }
 
   ngOnInit() {
--- src/app/game/services/game.service.spec.ts
+++ src/app/game/services/game.service.spec.ts
@@ -3,6 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { GameService } from './game.service';
 import { GameModule } from '../game.module';
 
+const getDescriptor = (obj, name) => {
+  return Object.getOwnPropertyDescriptor(obj.__proto__, name);
+};
+
 describe('GameService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [ GameModule ]
@@ -12,4 +16,75 @@ describe('GameService', () => {
     const service: GameService = TestBed.get(GameService);
     expect(service).toBeTruthy();
   });
+
+  it('should not mutate deck card list', () => {
+    const service: GameService = TestBed.get(GameService);
+    const deckBefore = service.deck;
+    service.buildFromDeck();
+    const deckAfter = service.deck;
+
+    expect(deckBefore).not.toBe(deckAfter);
+  });
+
+  it('should not mutate build card list', () => {
+    const service: GameService = TestBed.get(GameService);
+    service.buildFromDeck();
+
+    const buildingBefore = service.building;
+    service.backToDeck();
+    const buildingAfter = service.building;
+
+    expect(buildingBefore).not.toBe(buildingAfter);
+  });
+
+
+describe('GameService (Workshop)', () => {
+    let service: GameService;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [GameModule]
+      });
+
+      service = TestBed.get(GameService);
+    });
+
+    it('should have a getter deck', () => {
+      expect(getDescriptor(service, 'deck')).toBeTruthy();
+    });
+
+    xit('deck returns an empty array', () => {
+      expect(Array.isArray(service.deck)).toBeTruthy();
+      expect(service.deck.length).toBe(0);
+    });
+
+    it('should have a getter building', () => {
+      expect(getDescriptor(service, 'building')).toBeTruthy();
+    });
+
+    it('building returns an empty array', () => {
+      expect(Array.isArray(service.building)).toBeTruthy();
+      expect(service.building.length).toBe(0);
+    });
+
+    it('should have a getter game', () => {
+      expect(getDescriptor(service, 'game')).toBeTruthy();
+    });
+
+    it('game returns null', () => {
+      expect(service.game).toBe(null);
+    });
+
+    it('should have a method start', () => {
+      expect(typeof service.start).toBe('function');
+    });
+
+    it('should have a method reset', () => {
+      expect(typeof service.reset).toBe('function');
+    });
+
+
+  });
+
 });
+
+
