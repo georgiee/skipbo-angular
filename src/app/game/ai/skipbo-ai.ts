c80a396978afcad570c5d527621d70fb7e09e2dd
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-25 16:03:22 +0100

    provide observable and operator

--- src/app/game/ai/skipbo-ai.ts
+++ src/app/game/ai/skipbo-ai.ts
@@ -1,9 +1,9 @@
-import { Subject } from 'rxjs';
+import { interval, of, Subject } from 'rxjs';
 import { create as createSpy } from 'rxjs-spy';
 import { tag } from 'rxjs-spy/operators';
+import { delay, filter, last, mapTo, switchMap, take, tap, withLatestFrom } from 'rxjs/operators';
 import { Game, logger, Player } from 'skipbo-core';
-import { tap, switchMap, withLatestFrom, filter, mapTo, delay, mergeMap } from 'rxjs/operators';
-import { naivePlacementStrategy } from './placement-strategy';
+import { naivePlacementStrategyObservable } from './placement-strategy';
 
 
 createSpy({
@@ -27,7 +27,7 @@ export class SkipboAi {
     // allow manual trigger of a human auto turn
     this._playTurn.pipe(
       withLatestFrom(this._game.nextTurn, (_, player) => player),
-      switchMap(player => naivePlacementStrategy(player)),
+      switchMap(player => naivePlacementStrategyObservable(player)),
       tag('🐙: Manual Turn triggered')
     ).subscribe();
 
@@ -39,8 +39,19 @@ export class SkipboAi {
             filter(player => player.isCPU ),
             delay(500),
             tag('CPU Player takes turn - implement play here 🔽'),
-            // use `naivePlacementStrategy` somehow here
-            switchMap(player => naivePlacementStrategy(player).pipe(mapTo(player))),
+            switchMap(player =>
+                interval(500).pipe(
+                  tag('📖 interval counter'),
+                  take(5),
+                  switchMap(__ => of(player)
+                    .pipe(
+                      tag('📖 current player'),
+                    )
+                  ),
+                  mapTo(player),
+                  last()
+                )
+            ),
             tap((player: Player) => player.discardHandCard())
           )
       )
