e1a87a91c4ea4d5fa501e1dd61f656aedd38221e
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-25 17:45:11 +0100

    update with interval + takewhile

--- src/app/game/ai/skipbo-ai.ts
+++ src/app/game/ai/skipbo-ai.ts
@@ -1,14 +1,14 @@
 import { interval, of, Subject } from 'rxjs';
 import { create as createSpy } from 'rxjs-spy';
 import { tag } from 'rxjs-spy/operators';
-import { delay, filter, last, mapTo, switchMap, take, tap, withLatestFrom } from 'rxjs/operators';
+import { delay, filter, last, mapTo, switchMap, take, tap, withLatestFrom, takeWhile, defaultIfEmpty } from 'rxjs/operators';
 import { Game, logger, Player } from 'skipbo-core';
-import { naivePlacementStrategyObservable } from './placement-strategy';
+import { naivePlacementStrategyObservable, PlayerTryResult } from './placement-strategy';
 
 
-// createSpy({
-//   defaultPlugins: false
-// }).log(/interval counter|current player/);
+createSpy({
+  defaultPlugins: false
+}).log();
 
 
 export class SkipboAi {
@@ -41,14 +41,8 @@ export class SkipboAi {
             tag('CPU Player takes turn - implement play here 🔽'),
             switchMap(player =>
                 interval(500).pipe(
-                  tag('📖 interval counter'),
-                  take(5),
-                  switchMap(__ => naivePlacementStrategyObservable(player)
-                    .pipe(
-                      tag('📖 current player'),
-                    )
-                  ),
-                  mapTo(player),
+                  switchMap(__ => naivePlacementStrategyObservable(player)),
+                  tag('⏰ Interval'),
                   last()
                 )
             ),
