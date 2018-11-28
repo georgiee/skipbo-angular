4af2b8feff3066f31f5fa63ff74e3ae6bbc0f0cd
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-28 19:16:44 +0100

    first flip

--- src/app/game/components/flipcard/flipcard.component.ts
+++ src/app/game/components/flipcard/flipcard.component.ts
@@ -1,19 +1,50 @@
 import { Component, HostListener } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { trigger, state, style, transition, animation, animate } from '@angular/animations';
 
+// tslint:disable-next-line:interface-over-type-literal
+enum FlipState {
+  FRONT = 'front', // value side
+  BACK = 'back' // neutral side
+}
 @Component({
   selector: 'skipbo-flipcard',
   templateUrl: './flipcard.component.html',
   styleUrls: ['./flipcard.component.scss'],
-  animations: [ ]
+  animations: [
+    trigger('flipAnimation', [
+      state('front', style({
+        transform: 'rotateY(180deg)'
+      })),
+      state('back', style({
+        transform: 'rotateY(0deg)'
+      })),
+
+      transition('back => front', [
+        style({
+          transform: 'rotateY(0deg)'
+        }),
+        animate('500ms cubic-bezier(0.23, 1, 0.32, 1)',
+        style({
+          transform: 'rotateY(180deg)'
+        }))
+      ])
+    ])
+  ]
 })
 export class FlipCardComponent extends CardComponent {
+  flipState = FlipState.BACK;
+
   constructor() {
     super();
   }
 
   @HostListener('click')
   toggleFlip() {
-    console.log('toggleFlip');
+    if (this.flipState === FlipState.BACK) {
+      this.flipState = FlipState.FRONT;
+    } else {
+      this.flipState = FlipState.BACK;
+    }
   }
 }
