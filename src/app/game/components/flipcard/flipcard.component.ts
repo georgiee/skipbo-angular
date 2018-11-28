84fe346068721d7961686dc53c3b85d3e01492b6
Georgios Kaleadis <georgios@kaleadis.de>
2018-11-28 19:16:44 +0100

    first flip

--- src/app/game/components/flipcard/flipcard.component.ts
+++ src/app/game/components/flipcard/flipcard.component.ts
@@ -1,19 +1,35 @@
 import { Component, HostListener } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 
+// tslint:disable-next-line:interface-over-type-literal
+enum FlipState {
+  FRONT = 'front', // value side
+  BACK = 'back' // neutral side
+}
+
 @Component({
   selector: 'skipbo-flipcard',
   templateUrl: './flipcard.component.html',
   styleUrls: ['./flipcard.component.scss'],
-  animations: [ ]
+  animations: [
+
+  ]
 })
 export class FlipCardComponent extends CardComponent {
+  flipState = FlipState.BACK;
+
   constructor() {
     super();
   }
 
   @HostListener('click')
   toggleFlip() {
-    console.log('toggleFlip');
+    if (this.flipState === FlipState.BACK) {
+      this.flipState = FlipState.FRONT;
+    } else {
+      this.flipState = FlipState.BACK;
+    }
+
+    console.log('toggleFlip:', this.toggleFlip);
   }
 }
