b9c7c509522d1fa7375ff949186d5e0d76d99078
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-01 21:45:51 +0100

    disable all tests

--- src/app/game/components/flipcard/flipcard.component.ts
+++ src/app/game/components/flipcard/flipcard.component.ts
@@ -20,7 +20,7 @@ enum FlipState {
         transform: 'rotateY(0deg)'
       })),
 
-      transition('back => front, void => front', [
+      transition('back => front', [
         animate('500ms 250ms cubic-bezier(0.23, 1, 0.32, 1)',
           style({
             transform: 'rotateY(180deg)'
