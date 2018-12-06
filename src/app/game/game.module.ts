e2be87245410ca023cd4e5f9a304a11646db8984
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-06 11:55:45 +0100

    fix: remove already generated cli files to match the challenge

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameService } from './services/game.service';
-import { GameplayComponent } from './pages/gameplay/gameplay.component';
-import { CardComponent } from './components/card/card.component';
-import { CardFaceComponent } from './components/card-face/card-face.component';
-import { CardPileComponent } from './components/card-pile/card-pile.component';
 
 @NgModule({
-  declarations: [GameplayComponent, CardComponent, CardFaceComponent, CardPileComponent],
+  declarations: [],
   imports: [
     CommonModule
   ],
