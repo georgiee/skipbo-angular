086e09370aea8ac7d28ebf0fad38e4c478031395
Georgios Kaleadis <georgios@kaleadis.de>
2018-12-08 12:58:55 +0100

    build: disable hash for deployments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, {useHash: false})
   ],
   exports: [RouterModule]
 })
